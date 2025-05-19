eef90d5a6e2f16e9d3855d690e9b004e4f72e2f4
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-05-19 17:46:29 +0600

    добавление h2

--- src/main/java/sakhno/psup/product_service/ProductServiceApplication.java
+++ src/main/java/sakhno/psup/product_service/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package sakhno.psup.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories(basePackages = "sakhno.psup.product_service.repositories")
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
